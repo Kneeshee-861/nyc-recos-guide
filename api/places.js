23ccbc88382856dda5b9e11cba90288a8b55e2fc
Kneeshee-861 <nishi.intwala12@gmail.com>
2026-06-09 15:15:40 +0530

    debug: log full Google Places response

--- api/places.js
+++ api/places.js
@@ -8,13 +8,14 @@ module.exports = async function handler(req, res) {
     const response = await fetch('https://places.googleapis.com/v1/places:searchText', {
       method: 'POST',
       headers: {
-        'X-Goog-Api-Key': process.env.GOOGLE_PLACES_API_KEY,
+        'X-Goog-Api-Key': process.env.GOOGLE_API_KEY,
         'Content-Type': 'application/json',
         'X-Goog-FieldMask': 'places.rating,places.userRatingCount,places.priceLevel,places.types'
       },
       body: JSON.stringify({ textQuery: query })
     });
     const data = await response.json();
+    if (!data.places?.[0]) return res.status(200).json({ _debug: data });
     res.status(200).json(data.places?.[0] || {});
   } catch (err) {
     res.status(500).json({ error: err.message });
